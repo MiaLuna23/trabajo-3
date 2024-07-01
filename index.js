6ba66cfe20415a2f536629a0988da953bb85d800
MiaLuna23 <166016904+MiaLuna23@users.noreply.github.com>
2024-07-01 08:12:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+for (let indice = 10; indice > 0; indice = indice - 1) {
+    console.log(indice)
+}
+
+let edad = 15
+while (edad > 4) {
+    console.log(edad)
+    edad = edad - 1
+}
+
+let impar = 1
+do {
+    console.log(impar)
+    impar = impar + 2
+} while (impar < 101)
+
+for (let x = 1; x <= 10; x++) {
+    let resultado = 7 * x;
+    console.log(resultado);
+}
